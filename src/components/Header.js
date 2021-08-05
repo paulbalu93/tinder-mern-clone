f6d961209027a8c8c2725d38c82cecb1a590c6f7
Paul Maliakel <paulbalu93@gmail.com>
2021-08-05 02:01:12 +0200

    adds tindercard

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,21 @@
 import React from "react";
 import PersonIcon from "@material-ui/icons/Person";
+import ForumIcon from "@material-ui/icons/Forum";
 import IconButton from "@material-ui/core/IconButton";
-
+import "../css/Header.css";
 function Header() {
   return (
     <div className="header">
       <IconButton>
         <PersonIcon fontSize="large" className="header__icon" />
+      </IconButton>
+      <img
+        className="header__logo"
+        alt=""
+        src="https://w7.pngwing.com/pngs/399/756/png-transparent-tinder-logo-computer-icons-tinder-angle-logo-magenta-thumbnail.png"
+      />
+      <IconButton>
+        <ForumIcon fontSize="large" className="header__icon" />
       </IconButton>
     </div>
   );
